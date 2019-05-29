1846d3665f3068ea2c339f6d4b55c7c3730caa2f
Yoann <moise.yoann@gmail.com>
2019-05-29 14:20:32 +0200

    [ADD] Categories

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,40 @@ import WakaWakaVideo from '../assets/videos/waka_waka.mp4';
 import WakaWakaPoster from '../assets/images/waka_waka.png';
 import VideoPlayer from './VideoPlayer';
 
+const categories = [
+  {
+    name: 'Alpha',
+    image: WakaWakaPoster
+  },
+  {
+    name: 'Beta',
+    image: WakaWakaPoster
+  },
+  {
+    name: 'Gamma',
+    image: WakaWakaPoster
+  },
+  {
+    name: 'Epsilon',
+    image: WakaWakaPoster
+  },
+];
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <VideoPlayer poster={WakaWakaPoster} video={WakaWakaVideo} />
-      </header>
+    <header className="App-header">
+      {/*<VideoPlayer poster={WakaWakaPoster} video={WakaWakaVideo} />*/}
+
+      <div className='categories'>
+        {categories.map(({ name, image }) =>
+          <div className='category'>
+            <img src={image} className='categoryImage' />
+            <span className='categoryName'>{name}</span>
+          </div>
+        )}
+      </div>
+    </header>
 
 
     </div>
